36ca79a0c144155511fcadf9c9bab4f29a278819
saiprasad <rsaiprasad4@gmail.com>
2022-06-01 17:23:40 +0530

    DONE

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,107 +1,124 @@
 const userModel = require("../models/userModel")
 const orderModel = require("../models/orderModel")
+const cartModel = require("../models/cartModel")
+const productModel = require("../models/productModel")
 const validator = require('../validations/validator')
+const { isValid } = require("ipaddr.js")
 
 //------------------ PLACING OR CREATING AN ORDER
 const placeOrder = async (req, res) => {
 
     try {
 
-        // Extract userId From params
-        let userId = req.params.userId;
+        const userId = req.params.userId
 
-        // Validate the UserId
-        if (!validator.isValidobjectId(userId)) {
-            return res.status(400).send({ status: false, message: "Enter a Valid userID" })
-        }
-
-        // User Exists or not
-        const isUserExist = await userModel.findById(userId)
-        if (!isUserExist) {
-            return res.status(404).send({ status: false, message: "User not Found" });
+        if(! validator.isValidBody(userId)) {
+            return res.status(400).send({ status: false, message: 'Please enter valid user ID' });
         }
 
         // Authorization
-        if (userId != req.userId) {
-            return res.status(403).send({ status: false, message: "User not authorized to create a cart" })
+        // if(req.userId !== userId) {
+        //     return res.status(403).send({ status: false,message: `Authorisation failed; You are logged in as ${req.userId}, not as ${userId}`});
+        // }
+
+        const userDoc = await userModel.findById(userId) 
+
+        if(userDoc === null) {
+            return res.status(404).send({ status: false, message: 'User does not exist in DB' }); 
         }
 
-        // Check Req Body has some value or not ?
-        if (!validator.isValidBody(req.body)) {
+        let data = { ...req.body }; // req.body does not have a prototype; creating a new object (prototype object associates by default)
+
+        if (! validator.isValidBody(data)) {
             return res.status(400).send({ status: false, message: `Invalid Input Parameters` })
         }
+        
+        let { cartId, cancellable, ...remaining} = data
 
-        // Extract attributes from body by destructing
-        let { items, totalPrice, totalItems, totalQuantity } = req.body
-
-        // Check items are coming or not
-        if (!req.body.hasOwnProperty('items')) {
-            return res.status(400).send({ status: false, message: `Items Should Be present in request body` })
+        if(validator.isValidBody(remaining)){
+            return res.status(400).send({status: false, message: 'only cart ID is needed...'})
         }
 
-        // Items should be an array and its length should not be zero
-        if (!Array.isArray(items) || items.length == 0) {
+        if(cancellable && ! validator.isValidBoolean(cancellable)){
+            return res.status(400).send({status: false, message: 'please provide valid boolean value'})
+        }
+
+        const cart = await cartModel.findOne({_id: cartId, userId: userId}).select({items: 1 , totalPrice: 1, totalItems: 1})
+
+        if(! cart || cart.items.length == 0 ){
+            return res.status(404).send({ status: false, message: 'Cart does not exist or may be already Empty' });
+        }
+
+        const items = cart.items
+        const totalPrice = cart.totalPrice
+        const totalItems = cart.totalItems
+        let totalQuantity = null
+
+
+        if(!Array.isArray(items) || items.length == 0) {
             return res.status(400).send({ status: false, message: ` invalid input - Items` })
         }
 
-        // check product id and quantity for items are valid or not
-        for (let i = 0; i < items.length; i++) {
+        for(let i = 0 ; i < items.length ; i++) {
 
-            if (!validator.isValidobjectId(items[i].productId)) {
-                return res.status(400).send({ status: false, message: `${items[i].productId} is not a valid product id` })
+            let productId = items[i].productId
+
+            if(! validator.isValid(productId)){
+                return res.status(400).send({ status: false, message: `ProductId is empty`});
             }
 
-            if (!validator.isvalidNum(parseInt(items[i].quantity))) {
-                return res.status(400).send({ status: false, message: "Quantity should be a natural number" })   // "4" =>    Number()
+            if( ! validator.isValidobjectId(productId)) {
+                return res.status(400).send({ status: false, message: `ProductId is not valid Id`});
             }
+
+            let product = await productModel.findOne({ _id: productId , isDeleted: false})
+
+            if(!product) {
+                return res.status(404).send({ status: false, message: `product does not exist`});
+            }
+
+            let quantity = items[i].quantity // check later
+            if(isNaN(quantity) || quantity <= 0) {
+                return res.status(400).send({ status: false, message: `Quantity should be a minimum 1` }) 
+            }
+            totalQuantity += quantity
 
         }
 
-        // check Total Price is coming or not
-        if (!req.body.hasOwnProperty('totalPrice')) {
-            return res.status(400).send({ status: false, message: `total Price Should Be Presemt In Request Body` })
-        }
-
-        // Valid Total Price coming from body
-        if (!validator.isvalidNum(totalPrice)) {
+        if(isNaN(totalPrice) ){
             return res.status(400).send({ status: false, message: `Total Price Should Be A Number` })
         }
 
-        // check Total Items is coming or not
-        if (!req.body.hasOwnProperty('totalItems')) {
-            return res.status(400).send({ status: false, message: `total Price Should Be Present In Request Body` })
-        }
-
-        // Valid Total Items coming from body
-        if (!validator.isvalidNum(totalItems)) {
+        if (isNaN(totalItems)) {
             return res.status(400).send({ status: false, message: `Total items Should Be A Number` })
         }
 
-        // check Total Quantity is coming or not
-        if (!req.body.hasOwnProperty('totalQuantity')) {
-            return res.status(400).send({ status: false, message: `total Quantity Should Be Present In Request Body` })
-        }
 
-        // Valid Total Quantity coming from body
-        if (!validator.isvalidNum(totalQuantity)) {
-            return res.status(400).send({ status: false, message: `Total quantity Should Be A Number` })
-        }
+        // const totalQuantity = itemList.reduce( (acc,item) => acc +item.quantity, 0)
 
-
-        const newOrderData = {
+        let orderObj = {
             userId,
             items,
             totalPrice,
             totalItems,
-            totalQuantity
+            totalQuantity,
+            cancellable : cancellable
         }
+       
 
-        const newOrder = await orderModel.create(newOrderData)
-        res.status(201).send({ status: true, message: "Order Placed Successfully", data: newOrder })
+        const newOrder = await orderModel.create(orderObj)
 
+        const updated_Cart = await cartModel.findOneAndUpdate( {_id: cartId, userId : userId},
+            { $set : { items : [] , totalPrice : 0 , totalItems : 0} }, {new : true} 
+        )
+
+        return res.status(200).send({status: true, message: 'Order placed Successfully' , data: newOrder})
+
+        
     } catch (err) {
-
+        res.status(500).send({ status: false, message: "Internal Server Error", error: err.message });       
     }
+
 };
 
 
@@ -110,95 +127,77 @@ const updateOrderById = async (req, res) => {
 
     try {
 
-        // Extract User ID from params
-        let userId = req.params.userId.trim()
+        const userId = req.params.userId
 
-        // Validate the user ID
-        if (!validator.isValidobjectId(userId)) {
-            return res.status(400).send({ status: false, message: `${userId} is Not A Valid User Id` })
+        if(! validator.isValidobjectId(userId)) {
+            return res.status(400).send({ status: false, message: 'Please enter valid user ID' });
+        }
+        
+        const userDoc = await userModel.findById(userId) 
+
+        if(userDoc === null) {
+            return res.status(404).send({ status: false, message: 'User does not exist in DB' }); 
         }
 
-        // Check user Exists or not 
-        const isUserExist = await userModel.findById(userId)
-        if (!isUserExist) {
-            return res.status(404).send({ status: false, message: `userNot Found Please Check User Id` })
-        }
+        //  Authorization
+        // if(req.userId !== userId) {
+        //     return res.status(403).send({ status: false,message: `Authorisation failed; You are logged in as ${req.userId}, not as ${userId}`});
+        // }
 
-        // Authorization
-        if (isUserExist._id != req.userId) {
-            return res.status(403).send({ status: false, message: `Unauthorized Request !` })
-        }
-
-        // Check body is coming or not
-        if (!validator.isValidBody(req.body)) {
+        if (! validator.isValidBody(req.body)) {
             return res.status(400).send({ status: false, message: `Invalid Input Parameters` })
         }
+       
+        const data = { ...req.body }
 
-        // Destruct Request Body
-        let { orderId, status } = req.body
+        let { orderId, status } = data
 
-        // check Request Body Has Order Id or not 
-        if (!req.body.hasOwnProperty('orderId')) {
+        if(!data.hasOwnProperty('orderId')) {
             return res.status(400).send({ status: false, message: `Order Id Should Be Present In RequestBody` })
         }
 
-        // Check order id has value or not
-        if (!validator.isValid(orderId)) {
-            return res.status(400).send({ status: false, message: "Order id should be a valid string" })
-        }
-        orderId = orderId.trim()
-
-        // Validate the orderId
-        if (!validator.isValidobjectId(orderId)) {
-            return res.status(400).send({ status: false, message: `${orderId} is Not A Valid Object Id` })
+        if(! validator.isValid(orderId)){
+            return res.status(400).send({status: false, message: 'order Id is required!!!'})
         }
 
-        // Find order exists or not
-        const isValidOrder = await orderModel.findById(orderId)
-        if (!isValidOrder) {
-            return res.status(404).send({ status: false, message: `No Order Found By This ${orderId} id` })
+        if(! validator.isValidobjectId(orderId)){
+            return res.status(400).send({status: false, message: 'Please enter valid order Id '})
         }
 
-        // Authorization
-        if (isValidOrder.userId != userId) {
-            return res.status(403).send({ status: false, message: `order ${orderId} Does Not Belongs To ${userId} user` })
+        const isOrderExist = await orderModel.findOne({_id: orderId, isDeleted: false})
+
+        if(!isOrderExist){
+            return res.status(400).send({status: false, message:`Order not found for this user`})
         }
 
-        // Check request has status or not
-        if (!req.body.hasOwnProperty('status')) {
+        if(isOrderExist.userId.toString() !== userId) {
+            return res.status(400).send({status: false, message:`Order does not belong to user`})
+        }
+
+        if (!data.hasOwnProperty('status')) {
             return res.status(400).send({ status: false, message: `Status Should Be Present In Request Body` })
+    
         }
 
-        // Check request body is empty or not
-        if (!validator.isValid(status)) {
-            return res.status(400).send({ status: false, message: `Status should be a valid string` })
-        }
-        status = status.trim()
-
-        // Validate Status
-        if (!['completed', 'cancelled'].includes(status)) {
-            return res.status(400).send({ status: false, message: `Status can be changed from pending to "cancelled" or "completed" only` })
+        if(status && !["completed", "cancelled"].includes(status)) {
+            return res.status(400).send({status: false, message: `Status can be changed from pending to cancelled or completed only`})
         }
 
-        // Check status is completed or cancelled so we will not update that 
-        if (isValidOrder.status == 'completed' || isValidOrder.status == 'cancelled') {
-            return res.status(400).send({ status: false, message: `The order has been ${isValidOrder.status} allready` })
+        if(isOrderExist.status == 'completed' || isOrderExist.status == 'cancelled') {
+            return res.status(400).send({status: false, message: `Th order has been ${isOrderExist.status} already`})
         }
 
-        // then check for cancellable is false and status cancelled
-        if (isValidOrder.cancellable == false && status === 'cancelled') {
-            return res.status(400).send({ status: false, message: `Order Cannot Be Cancelled ` })
+        if(isOrderExist.cancellable == false && status == 'cancelled') {
+            return res.status(400).send({status: false, message: `Order can not be cancelled`})
         }
+        
+        const updatedData = await orderModel.findOneAndUpdate({_id: orderId}, {$set : {status: status}} , {new: true})
+        
+        return res.status(200).send({status: true, message:`order updated sucessfully` , data: updatedData})
 
-        // Finally update the order data
-        const updatedOrder = await orderModel.findOneAndUpdate({ _id: orderId },
-            { $set: { status: status } },
-            { new: true })
-
-        return res.status(200).send({ status: true, message: 'Order Updated Successfully', data: updatedOrder })
-
+        
     } catch (err) {
-
+        res.status(500).send({ status: false, message: err.message });        
     }
 };
 
