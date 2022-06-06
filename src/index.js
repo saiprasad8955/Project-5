64db63850c286dce07c9c5a64be2b603f688e1b0
saiprasad <rsaiprasad4@gmail.com>
2022-06-06 16:46:37 +0530

    dotenv added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const route = require("./route/route")
@@ -11,8 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(multer().any());
 
-
-mongoose.connect("mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/group8Database",
+console.log(process.env)
+mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true })
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
